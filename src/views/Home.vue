64ba28b801b0301319d347a62a8b13a5fdb6cd14
Sebastian Alvarez <sleam91@gmail.com>
2020-05-07 13:55:39 +0200

    added json and removed vue demo boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import books from '../assets/childrensbooks.json'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+
+  },
+  data(){
+    return{
+      books : books.books
+    }
   }
 }
 </script>
