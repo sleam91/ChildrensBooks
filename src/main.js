9a2592b82f703aba5f6a35d8a9d5aabe13cbe517
Sebastian Alvarez <sleam91@gmail.com>
2020-05-20 13:13:17 +0200

    added vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import books from './assets/childrensbooks.json'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-
   data() {
     return {
       books: books.books
@@ -21,6 +21,8 @@ new Vue({
       return this.books
     }
   },
+
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
